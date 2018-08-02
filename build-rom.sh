d970a0522e304616d54b6ea5db5f6da6ac721709
leoxia007 <38644173+leoxia007@users.noreply.github.com>
2018-08-02 13:45:02 +0800

    Update build-rom.sh

--- build-rom.sh
+++ build-rom.sh
@@ -88,12 +88,13 @@ buildVariant() {
 }
 
 repo manifest -r > release/$rom_fp/manifest.xml
-buildVariant treble_arm64_avN-userdebug arm64-aonly-vanilla-nosu
-buildVariant treble_arm64_agS-userdebug arm64-aonly-gapps-su
-buildVariant treble_arm64_bvN-userdebug arm64-ab-vanilla-nosu
-buildVariant treble_arm64_bgS-userdebug arm64-ab-gapps-su
-buildVariant treble_arm_avN-userdebug arm-aonly-vanilla-nosu
-buildVariant treble_arm_aoS-userdebug arm-aonly-gapps
+#buildVariant treble_arm64_avN-userdebug arm64-aonly-vanilla-nosu
+#buildVariant treble_arm64_agS-userdebug arm64-aonly-gapps-su
+#buildVariant treble_arm64_bvN-userdebug arm64-ab-vanilla-nosu
+#buildVariant treble_arm64_bgS-userdebug arm64-ab-gapps-su
+#buildVariant treble_arm_avN-userdebug arm-aonly-vanilla-nosu
+#buildVariant treble_arm_aoS-userdebug arm-aonly-gapp
+buildVariant treble_arm64_agN-userdebug arm64-aonly-gapps-nosu
 
 if [ "$release" == true ];then
     (
